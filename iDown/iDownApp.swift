b696f4c15c6e4f0a5f041a4af65322bc7d44fdc3
Rahul choudhary <heyitsmechoudhary@gmail.com>
2025-03-12 13:46:38 +0530

    Initial Commit

---
+++ iDown/iDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDownApp.swift
+//  iDown
+//
+//  Created by Rahul choudhary on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct iDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
